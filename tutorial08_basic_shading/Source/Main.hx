e451be5b549b17e73a8a1e794752b2641b04fcca
Carlos Madrazo <madrazo@bamtang.com>
2017-10-25 22:56:28 -0500

    Added tutorial 8
    needs fix suzanne.obj but shows cube.obj

--- tutorial08_basic_shading/Source/Main.hx
+++ tutorial08_basic_shading/Source/Main.hx
@@ -38,6 +38,10 @@ class Main extends Sprite {
         var fragShader:String = 
 "// Interpolated values from the vertex shaders
 " + Utils.IN() + " vec2 UV;
+" + Utils.IN() + " vec3 Position_worldspace;
+" + Utils.IN() + " vec3 Normal_cameraspace;
+" + Utils.IN() + " vec3 EyeDirection_cameraspace;
+" + Utils.IN() + " vec3 LightDirection_cameraspace;
 
 // Ouput data
 " + 
@@ -46,10 +50,53 @@ Utils.OUT_COLOR("color") +
 
 // Values that stay constant for the whole mesh.
 uniform sampler2D myTextureSampler;
+uniform mat4 MV;
+uniform vec3 LightPosition_worldspace;
 
 void main(){
-  // Output color = color of the texture at the specified UV
-  color = " + Utils.TEXTURE("myTextureSampler, UV") +";
+	// Light emission properties
+	// You probably want to put them as uniforms
+	vec3 LightColor = vec3(1,1,1);
+	float LightPower = 50.0f;
+	
+	// Material properties
+	vec3 MaterialDiffuseColor = texture( myTextureSampler, UV ).rgb;
+	vec3 MaterialAmbientColor = vec3(0.1,0.1,0.1) * MaterialDiffuseColor;
+	vec3 MaterialSpecularColor = vec3(0.3,0.3,0.3);
+
+	// Distance to the light
+	float distance = length( LightPosition_worldspace - Position_worldspace );
+
+	// Normal of the computed fragment, in camera space
+	vec3 n = normalize( Normal_cameraspace );
+	// Direction of the light (from the fragment to the light)
+	vec3 l = normalize( LightDirection_cameraspace );
+	// Cosine of the angle between the normal and the light direction, 
+	// clamped above 0
+	//  - light is at the vertical of the triangle -> 1
+	//  - light is perpendicular to the triangle -> 0
+	//  - light is behind the triangle -> 0
+	float cosTheta = clamp( dot( n,l ), 0.0 , 1.0);
+	
+	// Eye vector (towards the camera)
+	vec3 E = normalize(EyeDirection_cameraspace);
+	// Direction in which the triangle reflects the light
+	vec3 R = reflect(-l,n);
+	// Cosine of the angle between the Eye vector and the Reflect vector,
+	// clamped to 0
+	//  - Looking into the reflection -> 1
+	//  - Looking elsewhere -> < 1
+	float cosAlpha = clamp( dot( E,R ), 0.0, 1.0);
+	
+	vec3 col =
+		// Ambient : simulates indirect lighting
+		MaterialAmbientColor +
+		// Diffuse : color of the object
+		MaterialDiffuseColor * LightColor * LightPower * cosTheta / (distance*distance) +
+		// Specular : reflective highlight, like a mirror
+		MaterialSpecularColor * LightColor * LightPower * pow(cosAlpha,5.0) / (distance*distance);
+		
+	color = vec4(col,1.0); 
 }
 ";
 
@@ -60,15 +107,41 @@ void main(){
 Utils.IN(0) + " vec3 vertexPosition_modelspace;
 " +
 Utils.IN(1) + " vec2 vertexUV;
+" +
+Utils.IN(2) + " vec3 vertexNormal_modelspace;
 
 // Output data ; will be interpolated for each fragment.
 " + Utils.OUT() + " vec2 UV;
+" + Utils.OUT() + " vec3 Position_worldspace;
+" + Utils.OUT() + " vec3 Normal_cameraspace;
+" + Utils.OUT() + " vec3 EyeDirection_cameraspace;
+" + Utils.OUT() + " vec3 LightDirection_cameraspace;
+
 // Values that stay constant for the whole mesh.
 uniform mat4 MVP;
+uniform mat4 V;
+uniform mat4 M;
+uniform vec3 LightPosition_worldspace;
 
 void main(){
   // Output position of the vertex, in clip space : MVP * position
   gl_Position =  MVP * vec4(vertexPosition_modelspace,1.0);
+
+	// Position of the vertex, in worldspace : M * position
+	Position_worldspace = (M * vec4(vertexPosition_modelspace,1)).xyz;
+	
+	// Vector that goes from the vertex to the camera, in camera space.
+	// In camera space, the camera is at the origin (0,0,0).
+	vec3 vertexPosition_cameraspace = ( V * M * vec4(vertexPosition_modelspace,1)).xyz;
+	EyeDirection_cameraspace = vec3(0,0,0) - vertexPosition_cameraspace;
+
+	// Vector that goes from the vertex to the light, in camera space. M is ommited because it's identity.
+	vec3 LightPosition_cameraspace = ( V * vec4(LightPosition_worldspace,1)).xyz;
+	LightDirection_cameraspace = LightPosition_cameraspace + EyeDirection_cameraspace;
+	
+	// Normal of the the vertex, in camera space
+	Normal_cameraspace = ( V * M * vec4(vertexNormal_modelspace,0)).xyz; // Only correct if ModelMatrix does not scale the model ! Use its inverse transpose if not.
+	
 
   // The color of each vertex will be interpolated
   // to produce the color of each fragment
@@ -102,6 +175,8 @@ void main(){
 
         // Get a handle for our "MVP" uniform
         var matrixID = GL.getUniformLocation(prog, "MVP");
+        var viewMatrixID  = GL.getUniformLocation(prog, "V");
+        var modelMatrixID  = GL.getUniformLocation(prog, "M");
 
         m_controls = new Controls();
 
@@ -121,14 +196,23 @@ void main(){
         GL.bindBuffer(GL.ARRAY_BUFFER, uvbuffer);
         GL.bufferData(GL.ARRAY_BUFFER, new Float32Array(g_uv_buffer_data), GL.STATIC_DRAW);
 
+        var normalbuffer = GL.createBuffer();
+        GL.bindBuffer(GL.ARRAY_BUFFER, normalbuffer);
+        GL.bufferData(GL.ARRAY_BUFFER, new Float32Array(g_normals_buffer_data), GL.STATIC_DRAW);
 
         var posAttrib = 0;
 	var uvAttrib = 1;
+	var normalAttrib = 2;
         if (!Utils.isGLES3compat())
         {
           posAttrib = GL.getAttribLocation(prog, "vertexPosition_modelspace");
           uvAttrib = GL.getAttribLocation(prog, "vertexUV");
+	  normalAttrib = GL.getAttribLocation(prog, "vertexNormal_modelspace");
         }
+
+       // Get a handle for our "LightPosition" uniform
+        //GL.useProgram(prog);
+        var lightID  = GL.getUniformLocation(prog, "LightPosition_worldspace");
 
         ogl.render = function(rect:Rectangle)
         {
@@ -159,6 +243,11 @@ void main(){
             // Send our transformation to the currently bound shader, 
             // in the "MVP" uniform
             GL.uniformMatrix4fv(matrixID, false, Float32Array.fromMatrix(mvp));
+		GL.uniformMatrix4fv(modelMatrixID, false, Float32Array.fromMatrix(model));
+		GL.uniformMatrix4fv(viewMatrixID, false, Float32Array.fromMatrix(view));
+
+		var lightPos = new nme.geom.Vector3D(4,4,4);
+		GL.uniform3f(lightID, lightPos.x, lightPos.y, lightPos.z);
 
             // Bind our texture in Texture Unit 0
             GL.bindBitmapDataTexture( texture );
@@ -187,11 +276,24 @@ void main(){
                 0 // array buffer offset
                 );
 
+            // 3rd attribute buffer : normals
+            GL.enableVertexAttribArray(normalAttrib);
+            GL.bindBuffer(GL.ARRAY_BUFFER, normalbuffer);
+            GL.vertexAttribPointer(
+                normalAttrib, // attribute 
+                3, // size
+                GL.FLOAT, // type
+                false, // normalized?
+                0, // stride
+                0 // array buffer offset
+                );
+
 
             // Draw the triangles !
             GL.drawArrays(GL.TRIANGLES, 0, 12*3);
             GL.disableVertexAttribArray(0);
             GL.disableVertexAttribArray(1);
+            GL.disableVertexAttribArray(2);
 
             //NME: Disable if enabled per frame
             GL.disable(GL.DEPTH_TEST);
---
+++ tutorial08_basic_shading/Source/ObjLoader.hx
@@ -0,0 +1,88 @@
+
+package;
+
+import nme.Assets;
+
+
+class ObjLoader {
+    
+    //A *very* simple obj paser
+    //Alternatives: 
+    //assimp https://github.com/madrazo/haxe-assimp
+    //Away3D https://github.com/openfl/away3d/blob/4e3a686cb6a04f3ff16e602f54e3d53a8ea5c8de/away3d/loaders/parsers/OBJParser.hx
+    //tinyObj https://github.com/syoyo/tinyobjloader , GLM https://github.com/devernay/glm
+
+    static public function loadObj(objFile:String, vertexBufferData:Array<Float>, uvBufferData:Array<Float>, normalsBufferData:Array<Float>)
+    {
+        var objTxt:String = Assets.getText(objFile);
+        var lines:Array<String> = objTxt.split("\n");
+        var temp_vertices:Array<Float> = [];
+        var temp_uvs:Array<Float> = [];
+        var temp_normals:Array<Float> = [];
+        var vertexIndices:Array<Int> = [];
+        var uvIndices    :Array<Int> = [];
+        var normalIndices    :Array<Int> = [];
+
+        for(line in lines)
+        {
+          var words:Array<String> = line.split(" ");
+          if( words[0]=="v" )
+          {
+            temp_vertices.push(Std.parseFloat(words[1]));
+            temp_vertices.push(Std.parseFloat(words[2]));
+            temp_vertices.push(Std.parseFloat(words[3]));
+          }
+          else if( words[0]=="vt" )
+          {
+            temp_uvs.push(Std.parseFloat(words[1]));
+            temp_uvs.push(Std.parseFloat(words[2]));
+          }
+          else if( words[0]=="vn" )
+          {
+            temp_normals.push(Std.parseFloat(words[1]));
+            temp_normals.push(Std.parseFloat(words[2]));
+            temp_normals.push(Std.parseFloat(words[3]));
+          }
+          else if( words[0]=="f" )
+          {
+            var e0 = words[1].split("/");
+            var e1 = words[2].split("/");
+            var e2 = words[3].split("/");
+
+            vertexIndices.push(Std.parseInt(e0[0]));
+            vertexIndices.push(Std.parseInt(e1[0]));
+            vertexIndices.push(Std.parseInt(e2[0]));
+
+            uvIndices.push(Std.parseInt(e0[1]));
+            uvIndices.push(Std.parseInt(e1[1]));
+            uvIndices.push(Std.parseInt(e2[1]));
+
+            normalIndices.push(Std.parseInt(e0[2]));
+            normalIndices.push(Std.parseInt(e1[2]));
+            normalIndices.push(Std.parseInt(e2[2]));
+          }
+        }
+ 
+        for(i in 0...vertexIndices.length)
+        {
+            // Get the indices of its attributes
+            var vertexIndex = vertexIndices[i];
+            var uvIndex = uvIndices[i];
+            var normalIndex = normalIndices[i];
+
+            var v:Int = (vertexIndex-1)*3;
+            var t:Int = (uvIndex-1)*2;
+            var n:Int = (normalIndex-1)*3;
+            vertexBufferData.push(temp_vertices[ v ]);
+            vertexBufferData.push(temp_vertices[ v+1 ]);
+            vertexBufferData.push(temp_vertices[ v+2 ]);
+
+            uvBufferData.push(temp_uvs[ t ]);
+            uvBufferData.push(1.0-temp_uvs[ t+1 ]); //1.0- OK?
+
+            normalsBufferData.push(temp_vertices[ n ]);
+            normalsBufferData.push(temp_vertices[ n+1 ]);
+            normalsBufferData.push(temp_vertices[ n+2 ]);
+        }
+    }  
+}

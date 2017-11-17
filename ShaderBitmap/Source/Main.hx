cc8ef97cc18d66fa96b6ab0215f68d74728b9f38
Carlos Madrazo <madrazo@bamtang.com>
2017-11-17 12:30:00 -0500

    ShaderBitmap: Add no-texture example
    Add a heroku shader bitmap

--- ShaderBitmap/Source/Main.hx
+++ ShaderBitmap/Source/Main.hx
@@ -20,25 +20,36 @@ class Main extends Sprite {
 
         var bitmapData = Assets.getBitmapData ("assets/elephant1_Diffuse.png");
         var bitmapData_n = Assets.getBitmapData ("assets/elephant1_Normal.png");
+
+        //original bitmap
         var bitmap = new Bitmap ( bitmapData );
-        bitmap.x =  (stage.stageWidth - bitmap.width) / 2;
+        bitmap.x =  (stage.stageWidth - bitmap.width) / 2 -100;
         bitmap.y =  (stage.stageHeight - bitmap.height) / 2;
         addChild (bitmap);
 
         //Objects that use OGLView
         {
+            //no textures
+            var shaderProgram_texturemaping =  nme.gl.Utils.createProgram(vs, fs_heroku_6284_1);
+            var colorSquare:ShaderBitmap = new ShaderBitmap(shaderProgram_texturemaping, null, 100, 100);
+            colorSquare.x = ( (stage.stageWidth) / 2 ) -50;
+            colorSquare.y = (stage.stageHeight) / 2 + 80;
+
+            //1 texture
 	        var shaderProgram_texturemaping =  nme.gl.Utils.createProgram(vs, fs_texturemap);
 	        var colorElephant:ShaderBitmap = new ShaderBitmap(shaderProgram_texturemaping, [bitmapData]);
-	        colorElephant.x = ( (stage.stageWidth) / 2 ) -100;
+	        colorElephant.x = ( (stage.stageWidth) / 2 );
 	        colorElephant.y = (stage.stageHeight) / 2;
 
+            //2 textures
 	        var shaderProgram_bumpmaping =  nme.gl.Utils.createProgram(vs, fs_bumpmap);
 	        var bumpElephant:ShaderBitmap = new ShaderBitmap(shaderProgram_bumpmaping, [bitmapData, bitmapData_n]);
 	        bumpElephant.x = ( (stage.stageWidth) / 2) + 100;
 	        bumpElephant.y = (stage.stageHeight) / 2;
 
-	        addChild(bumpElephant);
+            addChild(colorSquare);
 	        addChild(colorElephant);
+            addChild(bumpElephant);
 	    }
     }
 
@@ -53,6 +64,28 @@ class Main extends Sprite {
         vTexCoord = texPosition;
         gl_Position = projectionMatrix * modelViewMatrix * vec4(vertexPosition, 1.0);
     }
+";
+
+
+//Pixel shader with no texture
+        public var fs_heroku_6284_1 = 
+"   varying vec2 vTexCoord;
+    uniform float time;
+    uniform vec2 mouse;
+    uniform vec2 resolution;
+  
+    void main() {           
+
+        vec2 position = ( gl_FragCoord.xy / resolution.xy ) + mouse / 1.0;
+            
+        float color = 0.0;
+        color += sin( position.x * cos( time / 15.0 ) * 80.0 ) + cos( position.y * cos( time / 15.0 ) * 10.0 );
+        color += sin( position.y * sin( time / 10.0 ) * 40.0 ) + cos( position.x * sin( time / 25.0 ) * 40.0 );
+        color += sin( position.x * sin( time / 5.0 ) * 10.0 ) + sin( position.y * sin( time / 35.0 ) * 80.0 );
+        color *= sin( time / 10.0 ) * 0.5;
+            
+        gl_FragColor = vec4( vec3( color, color * 0.1, sin( color + time / 9.0 ) * 0.75 ), 1.0 );
+    }  
 ";
 
 //Pixel shader with one texture
